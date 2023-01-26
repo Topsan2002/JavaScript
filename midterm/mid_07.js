29feea4aea96393cb2971270db030a12285194d1
topsan <topsan2002@gmail.com>
2023-01-26 11:01:35 +0700

    not fin mid_07.js

--- midterm/mid_07.js
+++ midterm/mid_07.js
@@ -5,27 +5,11 @@ sumTwoSmallNums([2, 9, 6, -1]);
 sumTwoSmallNums([879, 953, 694, -847, 342, 221, -91, -723, 791, -587])
 
 function sumTwoSmallNums(arr) {
-    let keep = [];
+    // const data = arr;
+    //use sort me !!!!!!! OK
     for (let val of arr) {
-        if (val >= 0) keep.push(val);
+
     }
-    // keep = 
-    keep.sort();
-    console.log(keep);
-    // return keep[0] + keep[1];
-    // let data = [];
-    // for (let i = 0; i < arr.length; i++) {
-    //     if (arr[i] < 0) {
-    //         arr.splice(i, 1)
-    //     };
-    // }
-    // for (let val of arr) {
-    //     if (val > 0 && data.length < 2) data.push(val);;
-    // }
-    // console.log(data[0] + data[1]);
-
-    // console.log(arr);
-
 }
 
 
